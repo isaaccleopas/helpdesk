036ee671ba39cbd36d30c5bef4013b337951457d
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2024-05-09 13:20:19 +0100

    finish Get Started

--- lib/helpdesk/support/ticket.ex
+++ lib/helpdesk/support/ticket.ex
@@ -1,15 +1,50 @@
 defmodule Helpdesk.Support.Ticket do
-  use Ash.Resource, domain: Helpdesk.Support
+  use Ash.Resource,
+    domain: Helpdesk.Support,
+    data_layer: Ash.DataLayer.Ets
 
   actions do
     defaults [:read]
 
-    create :create
+    create :open do
+      accept [:subject]
+    end
+  end
+
+  actions do
+    update :close do
+      accept []
+      validate attribute_does_not_equal(:statu, :closed) do
+        message "Ticket is already closed"
+      end
+
+      change set_attribute(:status, :closed)
+    end
+
+    update :assign do
+      accept [:representative_id]
+    end
   end
 
   attributes do
     uuid_primary_key :id
-    
-    attribute :subject, :string
+
+    attribute :subject, :string do
+      allow_nil? false
+
+      public? true
+    end
+
+    attribute :status, :atom do
+      constraints [one_of: [:open, :closed]]
+
+      default :open
+
+      allow_nil? false
+    end
+  end
+
+  relationships do
+    belongs_to :representative, Helpdesk.Support.Representative
   end
 end
